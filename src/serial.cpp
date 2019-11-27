15e99d748552db9db6ac9223a2baa95d495e1a16
Tony <chushuifurong618@126.com>
2019-11-27 10:17:07 +0800

    serial read use alloca

--- src/serial.cpp
+++ src/serial.cpp
@@ -93,7 +93,8 @@ void Serial::waitByteTimes(size_t count) {
   pimpl_->waitByteTimes(count);
 }
 
-int Serial::waitfordata(size_t data_count, uint32_t timeout, size_t *returned_size) {
+int Serial::waitfordata(size_t data_count, uint32_t timeout,
+                        size_t *returned_size) {
   return pimpl_->waitfordata(data_count, timeout, returned_size);
 }
 
@@ -108,19 +109,17 @@ size_t Serial::read(uint8_t *buffer, size_t size) {
 
 size_t Serial::read(std::vector<uint8_t> &buffer, size_t size) {
   ScopedReadLock lock(this->pimpl_);
-  uint8_t *buffer_ = new uint8_t[size];
+  uint8_t *buffer_ = static_cast<uint8_t *>(alloca(size * sizeof(uint8_t)));
   size_t bytes_read = this->pimpl_->read(buffer_, size);
   buffer.insert(buffer.end(), buffer_, buffer_ + bytes_read);
-  delete[] buffer_;
   return bytes_read;
 }
 
 size_t Serial::read(std::string &buffer, size_t size) {
   ScopedReadLock lock(this->pimpl_);
-  uint8_t *buffer_ = new uint8_t[size];
+  uint8_t *buffer_ = static_cast<uint8_t *>(alloca(size * sizeof(uint8_t)));
   size_t bytes_read = this->pimpl_->read(buffer_, size);
   buffer.append(reinterpret_cast<const char *>(buffer_), bytes_read);
-  delete[] buffer_;
   return bytes_read;
 }
 
@@ -144,7 +143,8 @@ size_t Serial::readline(string &buffer, size_t size, string eol) {
       break; // Timeout occured on reading 1 byte
     }
 
-    if (string(reinterpret_cast<const char *>(buffer_ + read_so_far - eol_len), eol_len) == eol) {
+    if (string(reinterpret_cast<const char *>(buffer_ + read_so_far - eol_len),
+               eol_len) == eol) {
       break; // EOL found
     }
 
@@ -207,7 +207,8 @@ vector<string> Serial::readlines(size_t size, string eol) {
 
 size_t Serial::write(const string &data) {
   ScopedWriteLock lock(this->pimpl_);
-  return this->write_(reinterpret_cast<const uint8_t *>(data.c_str()), data.length());
+  return this->write_(reinterpret_cast<const uint8_t *>(data.c_str()),
+                      data.length());
 }
 
 size_t Serial::write(const std::vector<uint8_t> &data) {
